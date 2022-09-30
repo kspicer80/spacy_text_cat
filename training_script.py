0b0ebcf77b3d24d3ef280001b001eec34ddfc87e
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-09-30 18:36:35 -0500

    trained models added

--- training_script.py
+++ training_script.py
@@ -1,6 +1,10 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
 from spacy.cli.train import train as spacy_train
-config_path = "/Users/kspicer/Desktop/spacy_textcat/config.cfg"
-output_model_path = "/Users/kspicer/Desktop/spacy_textcat/output"
+
+config_path = "config.cfg"
+output_model_path = "./output"
 spacy_train(
     config_path,
     output_path=output_model_path,
