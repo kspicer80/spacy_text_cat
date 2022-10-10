46ea3464f2e616b070a4e9bea8fe839411e7e344
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-10-10 10:31:39 -0500

    text_normalizatoion_script added

---
+++ text_normalization_function.py
@@ -0,0 +1,105 @@
+import spacy
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.tokenize.toktok import ToktokTokenizer
+import re
+from bs4 import BeautifulSoup
+from contractions import CONTRACTION_MAP
+import unicodedata
+nlp = spacy.load('en_core_web_lg', parse=True, tag=True, entity=True)
+tokenizer = ToktokTokenizer()
+stopword_list = nltk.corpus.stopwords.words('english')
+stopword_list.remove('no')
+stopword_list.remove('not')
+
+def strip_html_tags(text):
+    soup = BeautifulSoup(text, "html.parser")
+    stripped_text = soup.get_text()
+    return stripped_text
+
+def remove_accented_characters(text):
+    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    return text
+
+def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
+
+    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),
+                                      flags=re.IGNORECASE|re.DOTALL)
+    def expand_match(contraction):
+        match = contraction.group(0)
+        first_char = match[0]
+        expanded_contraction = contraction_mapping.get(match)\
+                                if contraction_mapping.get(match)\
+                                else contraction_mapping.get(match.lower())
+        expanded_contraction = first_char+expanded_contraction[1:]
+        return expanded_contraction
+
+    expanded_text = contractions_pattern.sub(expand_match, text)
+    expanded_text = re.sub("'", "", expanded_text)
+    return expanded_text
+
+def remove_special_characters(text, remove_digits=False):
+    pattern = r'[^a-zA-z0-9\s]' if not remove_digits else r'[^a-zA-z\s]'
+    text = re.sub(pattern, '', text)
+    return text
+
+def normalize_corpus(corpus, html_stripping=True, contraction_expansion=True,
+                     accented_char_removal=True, text_lower_case=True,
+                     text_lemmatization=True, special_char_removal=True,
+                     stopword_removal=True, remove_digits=True):
+
+    normalized_corpus = []
+    # normalize each document in the corpus
+    for doc in corpus:
+        # strip HTML
+        if html_stripping:
+            doc = strip_html_tags(doc)
+
+        try:
+          doc = doc.decode("utf-8-sig").replace(u"\ufffd", "?")
+        except:
+          doc = souped
+
+        # remove accented characters
+        if accented_char_removal:
+            doc = remove_accented_chars(doc)
+
+        # expand contractions
+        if contraction_expansion:
+            doc = expand_contractions(doc)
+
+        # lowercase the text
+        if text_lower_case:
+            doc = doc.lower()
+
+        # remove extra newlines
+        doc = re.sub(r'[\r|\n|\r\n]+', ' ',doc)
+
+        # lemmatize text
+        if text_lemmatization:
+            doc = lemmatize_text(doc)
+
+        # remove special characters and\or digits
+        if special_char_removal:
+            # insert spaces between special characters to isolate them
+            special_char_pattern = re.compile(r'([{.(-)!}])')
+            doc = special_char_pattern.sub(" \\1 ", doc)
+            doc = remove_special_characters(doc, remove_digits=remove_digits)
+
+            pat1 = r'@[A-Za-z0-9_]+'
+            pat2 = r'https?://[^ ]+'
+            www_pat = r'www.[^ ]+'
+            combined_pat = r'|'.join((pat1, pat2))
+            doc = re.sub(combined_pat, '', doc)
+            doc = re.sub(www_pat, '', doc)
+
+        # remove extra whitespace
+        doc = re.sub(' +', ' ', doc)
+        # remove stopwords
+        if stopword_removal:
+            doc = remove_stopwords(doc, is_lower_case=text_lower_case)
+
+        normalized_corpus.append(doc)
+
+    return normalized_corpus
