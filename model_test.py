8fe78667575d0aa017fe3bb276cfcc51578e1a3a
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-10-12 06:03:36 -0500

    updates

---
+++ model_test.py
@@ -0,0 +1,10 @@
+import json
+import spacy
+nlp = spacy.load("output/model-best")
+
+with open('./texts_for_testing/1880_90030.json', encoding="utf-8") as f:
+    test_text = json.load(f)
+opinion_of_text = test_text['html_lawbox']
+print(opinion_of_text)
+
+spacy_doc = nlp(opinion_of_text)
