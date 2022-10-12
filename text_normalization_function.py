e6781f89c5033e91faf5d17fc45405979d354ef1
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-10-12 12:44:26 -0500

    updates

--- text_normalization_function.py
+++ text_normalization_function.py
@@ -5,9 +5,9 @@ import nltk
 from nltk.tokenize.toktok import ToktokTokenizer
 import re
 from bs4 import BeautifulSoup
-from contractions import CONTRACTION_MAP
+from contractions import contractions_dict
 import unicodedata
-nlp = spacy.load('en_core_web_lg', parse=True, tag=True, entity=True)
+nlp = spacy.load('en_core_web_lg')
 tokenizer = ToktokTokenizer()
 stopword_list = nltk.corpus.stopwords.words('english')
 stopword_list.remove('no')
@@ -21,23 +21,6 @@ def strip_html_tags(text):
 def remove_accented_characters(text):
     text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
     return text
-
-def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
-
-    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),
-                                      flags=re.IGNORECASE|re.DOTALL)
-    def expand_match(contraction):
-        match = contraction.group(0)
-        first_char = match[0]
-        expanded_contraction = contraction_mapping.get(match)\
-                                if contraction_mapping.get(match)\
-                                else contraction_mapping.get(match.lower())
-        expanded_contraction = first_char+expanded_contraction[1:]
-        return expanded_contraction
-
-    expanded_text = contractions_pattern.sub(expand_match, text)
-    expanded_text = re.sub("'", "", expanded_text)
-    return expanded_text
 
 def remove_special_characters(text, remove_digits=False):
     pattern = r'[^a-zA-z0-9\s]' if not remove_digits else r'[^a-zA-z\s]'
@@ -56,25 +39,16 @@ def normalize_corpus(corpus, html_stripping=True, contraction_expansion=True,
         if html_stripping:
             doc = strip_html_tags(doc)
 
-        try:
-          doc = doc.decode("utf-8-sig").replace(u"\ufffd", "?")
-        except:
-          doc = souped
-
         # remove accented characters
         if accented_char_removal:
             doc = remove_accented_chars(doc)
-
-        # expand contractions
-        if contraction_expansion:
-            doc = expand_contractions(doc)
 
         # lowercase the text
         if text_lower_case:
             doc = doc.lower()
 
         # remove extra newlines
-        doc = re.sub(r'[\r|\n|\r\n]+', ' ',doc)
+        doc = re.sub(r'[\r|\n|\r\n]+', ' ', doc)
 
         # lemmatize text
         if text_lemmatization:
